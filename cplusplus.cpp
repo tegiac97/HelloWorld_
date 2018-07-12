15914f0e4f0e5f7cac9b5c7c7b31e8b75519c347
lebo <localhost@local.com>
2018-07-12 11:14:53 +0700

    Quan li nhan vien

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,66 +1,86 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int digitalRiver(int num);
-bool check(int a, int b);
+typedef struct quanLiFresher
+{
+	char MSNV;
+	char ten;
+	char account;
+	float diemTB;
+	struct quanLiFresher *next;
+};
+typedef quanLiFresher *list;
+
+quanLiFresher *makeNode(quanLiFresher*, char MSNV[10], char ten[20], char account[10], float diem);
+void themNhanVien(list &L, char MSNV[10], char ten[20], char account[10], float diem);
+int timTen(list l, char ten[20]);
+int timMSNV(list l, char MSNV[10]);
+int timDiem(list l, float diem);
+int timAccount(list l, char account[10]);
 
 int main() {
-	int a;
-	scanf("%d", &a);
-	int index = 1;
-	while (index < a) {
-		if (check(index, a) == 1) {
-			printf("YES\n");
-			break;
-		}
-		else if (check(index, a) == 0&&index==a-1) {
-			printf("NO\n");
-			break;
-		}
-		
-		index++;
-	}
-	//printf("%d", check(23, 24));
-	system("pause");
-}
-//int main() {
-//	int a;
-//	scanf("%d", &a);
-//	int index = 1;
-//	while (index < a) {
-//		if (check(a, index) == 1) {
-//			printf("YES\n");
-//			break;
-//		}
-//		else if (index == a && check(a,index) == 0)
-//			printf("NO\n");
-//		index++;
-//	}
-//	system("pause");
-//}
 
-int digitalRiver(int num)
-{
-	int plus = 0;
-	int temp = num;
-	while (temp > 0) {
-		plus += temp - temp / 10 * 10;
-		temp /= 10;
-	}
-	return num + plus;
 }
 
-bool check(int a, int b)
-{
-	int index = 1;
-	while (a != b&&index<2000) {
-		if (a < b) a = digitalRiver(a);
-		else if (a > b) b = digitalRiver(b);
-		else;
-		index++;
-	}
-	if (a == b) return 1;
-	else return false;
+quanLiFresher *makeNode(quanLiFresher* nhanVien, char MSNV[10], char ten[20], char account[10], float diem) {
+	nhanVien = ( quanLiFresher*) malloc(sizeof(quanLiFresher));
+	nhanVien->next = NULL;
+	nhanVien->MSNV = *MSNV;
+	nhanVien->ten = *ten;
+	nhanVien->account = *account;
+	nhanVien->diemTB = diem;
+	return nhanVien;
 }
 
+void themNhanVien(list &L, char MSNV[10], char ten[20], char account[10], float diem) {
+	quanLiFresher *nhanVien;
+	nhanVien = makeNode(nhanVien, MSNV, ten, account, diem);
+	nhanVien->next = L;
+	L = nhanVien;
+}
 
+int timTen(list l, char ten[20]) {
+	quanLiFresher *nhanVien = l;
+	int i = 1;
+	while (nhanVien != NULL && nhanVien->ten != *ten) {
+		nhanVien = nhanVien->next;
+		i++;
+	}
+	if (nhanVien != NULL) return i;
+	else return 0;
+}
+
+int timMSNV(list l, char MSNV[10]) {
+	quanLiFresher *nhanVien = l;
+	int i = 1;
+	while (nhanVien != NULL && nhanVien->MSNV != *MSNV) {
+		nhanVien = nhanVien->next;
+		i++;
+	}
+	if (nhanVien != NULL) return i;
+	else return 0;
+}
+
+int timDiem(list l, float diem) {
+	quanLiFresher *nhanVien = l;
+	int i = 1;
+	while (nhanVien != NULL && nhanVien->diemTB != diem) {
+		nhanVien = nhanVien->next;
+		i++;
+	}
+	if (nhanVien != NULL) return i;
+	else return 0;
+}
+
+int timAccount(list l, char account[10]) {
+	quanLiFresher *nhanVien = l;
+	int i = 1;
+	while (nhanVien != NULL && nhanVien->account != *account) {
+		nhanVien = nhanVien->next;
+		i++;
+	}
+	if (nhanVien != NULL) return i;
+	else return 0;
+}
+
+void xoaMSNV(list l, char msnv[10], char )
